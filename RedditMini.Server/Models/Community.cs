ad147cd6ab0f3f13865cd8b4620ce3cad1986d3e
Kurtić Džemal <Dzemaludeen@gmail.com>
2026-09-24 17:10:12 +0200

    Add a Community and a Post entity

---
+++ RedditMini.Server/Models/Community.cs
@@ -0,0 +1,10 @@
+﻿namespace RedditMini.Server.Models;
+
+public class Community
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public List<Post> Posts { get; set; } = [];
+}
