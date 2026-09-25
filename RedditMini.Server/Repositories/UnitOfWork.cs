e92d94f11748b00027bbad90ebf5f4ffafa3d718
Kurtić Džemal <Dzemaludeen@gmail.com>
2026-09-25 18:46:27 +0200

    implement unitofwork

---
+++ RedditMini.Server/Repositories/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using RedditMini.Server.Data;
+
+namespace RedditMini.Server.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly RedditMiniDbContext _dbContext;
+    public UnitOfWork(RedditMiniDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
